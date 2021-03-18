58c103f9d1ae8c8c4fcd5d351184bca159732710
gbp12 <gonzabonilla4896@outlook.es>
2021-03-18 23:03:33 +0000

    saludos

---
+++ clase1.js
@@ -0,0 +1,19 @@
+var gonzalo = {
+    nombre: "gonzalo",
+    edad: 19
+}
+
+var helena = {
+    nombre: "helena",
+    edad: 25
+}
+
+function saludo(nombre) {
+   
+    console.log("Hola " + nombre.nombre + " tienes " + nombre.edad + " años");
+}
+
+
+
+saludo(gonzalo)
+saludo(helena)
